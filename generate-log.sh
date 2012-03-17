5417d29e82b269839006b396e7862c8088783e6b
Frédéric Sagnes <speedup@gmail.com>
2012-03-17 23:14:45 +0100

    Make sure we get the app's debug logs in the core data log

--- generate-log.sh
+++ generate-log.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
 
 cd `dirname $0`
-xcodebuild -target CoreData-Mac SYMROOT=build
-./build/Release/CoreData-Mac -com.apple.CoreData.SQLDebug 1 &> CoreData-Mac.log
+xcodebuild -target CoreData-Mac -configuration Debug SYMROOT=build
+./build/Debug/CoreData-Mac -com.apple.CoreData.SQLDebug 1 &> CoreData-Mac.log
